7b4758f006c0b4cbb0dfedffc8b4f412ca2e5ff2
Rex Miller <miller.rex@gmail.com>
2015-07-01 22:25:22 -0700

    Fixed project/solution configuraiton/build issues

---
+++ IoTheMan.Web/Controllers/PersonPaymentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IoTheMan.Web.Controllers
+{
+    public class PersonPaymentController : Controller
+    {
+        [Route("person/{id}/payment/create")]
+        public ActionResult Create(string id)
+        {
+            return View();
+        }
+    }
+}
